b3b91543b20bc74bcfb35f1437ddb03cd7e3ec43
Tri <20520327@gm.uit.edu.vn>
2021-12-02 23:51:27 +0700

    Chỉnh sửa file SQL

--- SQL_quan_li/Quanli_quanan.sql
+++ SQL_quan_li/Quanli_quanan.sql
@@ -32,15 +32,10 @@ create table EMPLOYEE
 )
 go
 ALTER TABLE EMPLOYEE ALTER COLUMN FULLNAME NVARCHAR(200) COLLATE Vietnamese_CI_AS
-alter table EMPLOYEE add constraint CHK_SEX CHECK (SEX in(N'Nam',N'Nữ'))
-alter table EMPLOYEE drop CHK_SEX
+
 insert into EMPLOYEE(ID,FULLNAME,POSITION,ADDRESS,PHONE,SEX,EMAIL) values ('1',N'Trần Hữu Trí',N'Người quản lí',N'95/20/18B Lê Văn Lương, Tân Kiểng, Quận 7','0857350234',N'Nam',N'tritran5789@gmail.com');
-select * from EMPLOYEE
-drop table EMPLOYEE
 
-select * from EMPLOYEE where FULLNAME LIKE N'Trần %'LIKE N't%';
 
-alter table EMPLOYEE alter column FULLNAME nvarchar(max) COLLATE Vietnamese_CI_AS
 insert into EMPLOYEE(ID,FULLNAME,POSITION,ADDRESS,PHONE,SEX,EMAIL) values ('2','Trần Hữu Trí','Nhân viên','95/20/18B Lê Văn Lương, Tân Kiểng, Quận 7','0857350234','Nam','tritran5789@gmail.com');
 insert into EMPLOYEE(ID,FULLNAME,POSITION,ADDRESS,PHONE,SEX,EMAIL) values ('7','Trần Hữu Trí','Nhân viên','95/20/18B Lê Văn Lương, Tân Kiểng, Quận 7','0857350234','Nam','tritran5789@gmail.com');
 insert into EMPLOYEE(ID,FULLNAME,POSITION,ADDRESS,PHONE,SEX,EMAIL) values ('8','Trần Hữu Trí','Nhân viên','95/20/18B Lê Văn Lương, Tân Kiểng, Quận 7','0857350234','Nam','tritran5789@gmail.com');
@@ -59,13 +54,7 @@ create table ACCOUNT
 )
 go
 
-select * from ACCOUNT
-
 INSERT INTO ACCOUNT(EMPLOYEEid,ROLEid,USERNAME,PASSWORD) values ('1','0','trihuu15','123456');
-select * from ACCOUNT WHERE USERNAME = N'trihuu15' AND PASSWORD = N'123456'
-
-delete ACCOUNT  where EMPLOYEEid = '1' AND ROLEid = '0'
-
 
 create table CATEGORY
 (
@@ -73,10 +62,6 @@ create table CATEGORY
 	NAME		nvarchar(500)
 )
 go
-
-select A.ID, A.NAME, B.NAME, A.PRICE from FOOD A inner join CATEGORY B on A.CATEid = B.ID
-
-select ID from CATEGORY where NAME = N'Nước uống'
 
 insert into CATEGORY (ID, NAME) values ('1',N'Nước uống')
 insert into CATEGORY (ID, NAME) values ('2',N'Cơm')
@@ -89,13 +74,12 @@ create table FOOD
 (
 	ID			integer primary key,
 	NAME		nvarchar(max),
-	PRICE		money,
+	PRICE		integer,
 	CATEid		integer
 	
 	FOREIGN KEY (CATEid) REFERENCES CATEGORY(ID)
 )
 go
-alter table FOOD alter column PRICE integer
 
 insert into FOOD (ID, NAME, CATEid, PRICE) values ('2',N'Cơm bò xào','2','35000')
 insert into FOOD (ID, NAME, CATEid, PRICE) values ('3',N'Cơm chiên cá mặn','2','35000')
@@ -104,8 +88,6 @@ insert into FOOD (ID, NAME, CATEid, PRICE) values ('5',N'Rau muống xào','4','
 insert into FOOD (ID, NAME, CATEid, PRICE) values ('6',N'Bò xào chua ngọt','5','60000')
 insert into FOOD (ID, NAME, CATEid, PRICE) values ('7',N'Mực xào cải ngọt','5','80000')
 
-select A.ID, A.NAME, B.NAME, A.PRICE from FOOD A inner join CATEGORY B on A.CATEid = B.ID where A.NAME LIKE N'C%'
-
 create table TABLEQA
 (
 	ID			integer primary key,
@@ -113,11 +95,6 @@ create table TABLEQA
 	STATUS		nvarchar(50)
 )
 go
-alter table TABLEQA add constraint CHK_STATUS CHECK (STATUS in (N'Bàn trống',N'Có người'))
-alter table TABLEQA drop CHK_STATUS
-update TABLEQA
-SET ID = '13', NAME = N'Bàn 14', STATUS = N'Bàn trống'
-where ID = 12
 
 insert into TABLEQA (ID, NAME, STATUS) values ('1',N'Bàn 1',N'Bàn trống')
 insert into TABLEQA (ID, NAME, STATUS) values ('2',N'Bàn 2',N'Bàn trống')
@@ -133,10 +110,6 @@ insert into TABLEQA (ID, NAME, STATUS) values ('11',N'Bàn 11',N'Bàn trống')
 insert into TABLEQA (ID, NAME, STATUS) values ('12',N'Bàn 12',N'Bàn trống')
 insert into TABLEQA (ID, NAME, STATUS) values ('13',N'Bàn 13',N'Bàn trống')
 insert into TABLEQA (ID, NAME, STATUS) values ('14',N'Bàn 14',N'Bàn trống')
-
-
-
-drop table TABLEQA
 
 create table ORDER_QA
 (
@@ -184,23 +157,3 @@ update TABLEQA
 set STATUS = N'Có người'
 where ID in ((select TABLEid from ORDER_QA where BILLstatus = '0'))
 
-select * from ORDER_QA
-select * from REVENUE
-
-delete 
-
-select A.ORDERid,B.TABLEid,A.TOTAL,A.CHECKIN,A.CHECKOUT 
-from REVENUE A INNER JOIN ORDER_QA B ON A.ORDERid = B.ID 
-WHERE (A.CHECKOUT BETWEEN '12/2/2021' AND '12/3/2021') AND B.BILLstatus = '1'
-
-select A.ORDERid,B.TABLEid,A.TOTAL,A.CHECKIN,A.CHECKOUT 
-from REVENUE A INNER JOIN ORDER_QA B ON A.ORDERid = B.ID 
-WHERE A.CHECKOUT BETWEEN '12/1/2021' AND '12/2/2021'
-
-select A.ORDERid,B.TABLEid,A.TOTAL,A.CHECKIN,A.CHECKOUT 
-from REVENUE A INNER JOIN ORDER_QA B ON A.ORDERid = B.ID 
-WHERE A.CHECKOUT BETWEEN '12/1/2021' AND '12/2/2021'
-
-select A.ORDERid,B.TABLEid,A.TOTAL,A.CHECKIN,A.CHECKOUT from REVENUE A INNER JOIN ORDER_QA B ON A.ORDERid = B.ID WHERE (A.CHECKOUT BETWEEN '12/2/2021' AND '12/3/2021') AND B.BILLstatus = '1'
-
-select A.ORDERid,B.TABLEid,A.TOTAL,A.CHECKIN,A.CHECKOUT from REVENUE A INNER JOIN ORDER_QA B ON A.ORDERid = B.ID WHERE (A.CHECKOUT BETWEEN '12/2/2021 12:00:00 AM' AND '12/3/2021 12:00:00 AM') AND B.BILLstatus = '1'
